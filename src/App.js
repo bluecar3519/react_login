ab240bc7ea0e40aee94941b6c5d8cec53499ff1c
bluecar3519 <112738709+bluecar3519@users.noreply.github.com>
2024-09-05 06:45:08 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Login from "./Login";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <Login />
+    </div>
+  );
+}
